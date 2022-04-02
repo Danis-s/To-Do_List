f6ad23fb529cfc3e10fa9d9df1688b435cc84467
Andrei Holub <holubgetrekt@gmail.com>
2022-04-02 18:17:15 +0200

    Docker-compose added

--- src/main/java/com/example/demo/Controllers/ToDoListController.java
+++ src/main/java/com/example/demo/Controllers/ToDoListController.java
@@ -1,8 +1,10 @@
 package com.example.demo.Controllers;
 
+import com.example.demo.Models.ToDoList;
 import com.example.demo.Services.ToDoListService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
